3066cc70d69d42137f16cba428326e125ad116ee
Roman Zhuravel <zhooravell@gmail.com>
2018-08-17 00:25:04 +0300

    init

---
+++ src/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!function_exists('getThreeCharacterCountries')) {
+    /**
+     * @return array
+     */
+    function getThreeCharacterCountries()
+    {
+        return include __DIR__.'../resources/iso-3166-1-alpha-3.php';
+    }
+}
+
+if (!function_exists('getTwoCharacterCountries')) {
+    /**
+     * @return array
+     */
+    function getTwoCharacterCountries()
+    {
+        return include __DIR__.'../resources/iso-3166-1-alpha-2.php';
+    }
+}
